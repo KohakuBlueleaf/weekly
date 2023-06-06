d3f84176ba7674fe76baba90ca7dd28f20c99c6e
Kohaku-Blueleaf <59680068+KohakuBlueleaf@users.noreply.github.com>
2023-06-06 16:33:45 +0800

    Add user middleware

--- src/server/app.js
+++ src/server/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const userHandler = require('./middleware/user-login');
 const errorHandler = require('./middleware/error-handler');
 const requestLogger = require('./middleware/request-logger');
 const accessController = require('./middleware/access-controller');
 
 app.use(errorHandler);
 app.use(accessController);
+app.use(userHandler);
 if(process.env.NODE_ENV === 'development') {
   app.use(requestLogger);
 }
--- src/server/routes/user.js
+++ src/server/routes/user.js
@@ -7,32 +7,7 @@ const router = express.Router();
 
 // 處理 GET /api/user 路由
 router.get('/', async (req, res) => {
-  let idToken = req.get("idToken");
-  let payload = (await awsUtils.verifyToken(idToken));
-  
-  let checksum = payload.aud;
-  if(checksum != awsUtils.cliId){
-    res.json({message: 'Invalid Login Token'})
-    return
-  }
-  
-  let uid = payload.sub;
-  let username = payload['cognito:username'];
-  let email = payload.email;
-  
-  let user = await database.User.findOne({
-    where: {
-      uid: uid,
-    }
-  })
-  if(user == null){
-    user = await database.User.create({
-      uid: uid, 
-      username: username,
-      email: email
-    });
-  }
-  
+  let user = req.userData;
   console.log(new Date().toISOString(), ":", `${user.uid}:${user.username}`, "has logged in");
   res.json({ message: 'Welcome to the user API!' });
 });
