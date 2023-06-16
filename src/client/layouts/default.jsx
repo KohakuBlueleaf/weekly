7e6b058bc8cc830834e8f158469b235cc5d0f9e3
YaHey <y910906@gmail.com>
2023-06-17 03:20:43 +0800

    filter

--- src/client/layouts/default.jsx
+++ src/client/layouts/default.jsx
@@ -42,9 +42,6 @@ const DefaultLayout = () => {
 
   return (
     <Container fluid className='m-0 p-0 h-100 d-flex flex-column flex-row'>
-=======
-    <Container fluid className='m-0 p-0 h-100 d-flex flex-column'>
->>>>>>> 9084200d9885279c7e292a5fd37c31e084176aae
       <TitleBar></TitleBar>
       <div className='m-4'>
         <h1>Default Layout</h1>
