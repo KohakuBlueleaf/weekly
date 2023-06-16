3412f77ea9fe864ef30b1d6e9c1fa95e16c830b5
mike911209 <mike911209@gmail.com>
2023-06-17 03:48:00 +0800

    TimeLine-Grid customize

--- src/client/components/TimeLineMonth.jsx
+++ src/client/components/TimeLineMonth.jsx
@@ -8,7 +8,7 @@ const TimeLineMonth = (props) => {
   const dispatch = useDispatch();
 
   return (
-    <div className='d-flex col-1 p-0 align-items-end'>
+    <div className='d-flex TimeLineMonth-col p-0 align-items-end'>
         <p className='p-0 m-0'>{props.month}</p>
     </div>
   );
