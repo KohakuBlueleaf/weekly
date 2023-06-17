4d34e18253417087fe530a04e3654c1ab8ba80f5
mike911209 <mike911209@gmail.com>
2023-06-17 22:14:58 +0800

    help modal

--- src/client/components/Navbar.jsx
+++ src/client/components/Navbar.jsx
@@ -19,7 +19,9 @@ import { addToggle } from '../store/homePage/action';
 import { addToggle as addToggle_event } from "../store/event/action"
 import { tagsAddToggle } from '../store/tags/action';
 import { addToggle as todoAddToggle } from '../store/todo/action';
+import { helpToggle } from '../store/help/action';
 import ManTab from './ManTab';
+import HelpModal from './HelpModal';
 
 const OffcanvasExample = (props) => {
   const navigate = useNavigate();
@@ -63,6 +65,10 @@ const OffcanvasExample = (props) => {
     dispatch(navClose());
   }
 
+  const hadleHelpClick = () => {
+    dispatch(helpToggle());
+  }
+
   return (
     <Navbar bg="light" expand={false} className="mt-auto navbar">
       <Container fluid className='d-flex flex-row'>
@@ -96,7 +102,7 @@ const OffcanvasExample = (props) => {
               <Link className="nav-link" to='/' onClick={navclose}>Home</Link>
               <Link className="nav-link" to='/management' onClick={navclose}>Management</Link>
               <Link className="nav-link" to='/tags' onClick={navclose}>Tags</Link>
-              <Link className="nav-link" to='/helps' onClick={navclose}>Helps</Link>
+              <Link className="nav-link" onClick={hadleHelpClick}>Helps</Link>
               <Link className="nav-link" to='/settings' onClick={navclose}>Settings</Link>
             </Nav>
             {button}
