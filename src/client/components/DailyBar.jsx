b835a20e34f45976243be61a2e94886917b1684a
YaHey <y910906@gmail.com>
2023-06-18 00:12:23 +0800

    timebar

--- src/client/components/DailyBar.jsx
+++ src/client/components/DailyBar.jsx
@@ -1,11 +1,34 @@
 import { Style } from 'maplibre-gl';
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const DailyBar = () => {
-  const now = 60;
-  return <ProgressBar now={now} label={`${now}%`}/>;
+    const date = new Date();
+    let display_year = date.getFullYear();
+    let display_month = date.getMonth();
+    let display_day = date.getDate(); 
+    const now = 60;
+    return (
+        <span>
+            <Container fluid>
+                <Row className='d-flex'>
+                    <Col xs={3} md={2} lg={1}>
+                        {display_year}.{display_month}.{display_day}
+                    </Col>    
+                    <Col xs={9} md={10} lg={11}>
+                        <ProgressBar now={now} label={`${now}%`}/>
+                    </Col>
+                    
+                </Row>
+                
+                
+            </Container>
+            
+        </span>
+    ) 
+    
 }
 
 export default DailyBar;
