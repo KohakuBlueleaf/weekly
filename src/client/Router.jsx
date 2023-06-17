4b31ef365f0cf0634b4f8cf322db0180f4e590d8
YaHey <y910906@gmail.com>
2023-06-17 16:29:32 +0800

    re management

--- src/client/Router.jsx
+++ src/client/Router.jsx
@@ -3,10 +3,10 @@ import { Routes, BrowserRouter, Route } from 'react-router-dom';
 
 import DefaultLayout from './layouts/default';
 import EmptyLayout from './layouts/empty';
-import Management from './pages/Management';
 import Home from './pages/Home';
 import Login from './pages/Login'
 import Settings from './pages/Settings';
+import Event from './pages/Event';
 import Routine from './pages/Routine';
 import Todo from './pages/Todo';
 import Tags from './pages/Tags';
@@ -20,9 +20,9 @@ const Router = () => {
 
         <Route element={<DefaultLayout/>}>
           <Route index={true} path="" element={<Home />} />
-          <Route path="/management" element={<Management/>} />
-            <Route path="/routine" element={<Routine/>} />
-            <Route path="/todo" element={<Todo/>} />
+          <Route path="/management" element={<Event/>} />
+            <Route path="/management/routine" element={<Routine/>} />
+            <Route path="/management/todo" element={<Todo/>} />
           <Route path="/tags" element={<Tags/>} />
           {/* <Route path="/helps" element={<Help />} /> */}
           <Route path="settings" element={<Settings />} />
