b570b8fd35ab813077abba9c03c4aec3c03ef274
mike911209 <mike911209@gmail.com>
2023-06-20 04:04:47 +0800

    daily

--- src/client/components/DailyTree.jsx
+++ src/client/components/DailyTree.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
+import { useOutletContext, useNavigate } from "react-router-dom";
+import { listEvents as listEventsFromApi } from '../api/event';
+import { getPageDate } from '../utils';
+import { endListEvents } from '../store/posts/action';
 
 const DailyTree = () => {
     const dispatch = useDispatch();
 
-    const {
-        event,
-    } = useSelector((state) => ({
-        event: state.addModal.event
-    }));
+    const loginStatus = useSelector((state) => state.user.token);
+    const [user, authStatus] = useOutletContext();
+    const listEvents = useSelector((state) => state.addModal.event);
+
+    useEffect(() => {
+        (async () => {
+            let temp;
+            temp = await listEventsFromApi(getPageDate(), loginStatus);
+            dispatch(endListEvents(temp));
+            // console.log("temp: askldhjklasdhklasdhklhasdsd", temp);
+            // console.log('get events', listEvents);
+        })
+    }, [listEvents, loginStatus])
     
+    console.log("events: ~~~~~~~~~~~~~~~~~", listEvents[2]);
     return (
         //插入圖片(routine/events/樹幹)
-        <div>
-            <div className='d-flex justify-content-end'>
-                ss quiz
-            </div>
-            <div className='d-flex justify-content-start'>
-                English
-            </div>
-            <div className='d-flex justify-content-start'>
-                English
-            </div>
-            <div className='d-flex justify-content-end'>
-                ss lab
-            </div>
-            <div className='d-flex justify-content-start'>
-                python
-            </div>
+        <div className='container d-flex flex-column'>
+            {
+                listEvents.map((item, index) => {
+                    <div key={item.id} className='d-flex flex-row'>
+                        <div className='col-6'>
+                            {item.type === 'routine' ? `${item.title}` : ''}
+                        </div>
+                        <div className='col-6'>
+                            {item.type === 'event' ? `${item.title}` : ''}
+                        </div>
+                    </div>
+                })
+            }
         </div>    
         
     ) 
