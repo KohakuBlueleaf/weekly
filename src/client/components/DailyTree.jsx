f52296cb3f43195f0d2a225aaba0eccf2da8b190
mike911209 <mike911209@gmail.com>
2023-06-20 05:00:06 +0800

    daily display left and right

--- src/client/components/DailyTree.jsx
+++ src/client/components/DailyTree.jsx
@@ -3,7 +3,7 @@ import { Container, Row, Col } from 'react-bootstrap';
 import { useSelector, useDispatch } from 'react-redux';
 import { useOutletContext, useNavigate } from "react-router-dom";
 import { listEvents as listEventsFromApi } from '../api/event';
-import { getPageDate } from '../utils';
+import { getPageDate, getToday } from '../utils';
 import { endListEventAll } from '../store/event/action';
 
 async function getEventList(PageDate, login) {
@@ -36,31 +36,37 @@ const DailyTree = () => {
         })();
     },[loginStatus, authStatus]);
     
-    // console.log("events: ~~~~~~~~~~~~~~~~~", listEvents[2]);
     return (
-        //插入圖片(routine/events/樹幹)
-        <div className='container d-flex flex-column'>
+        <div className='d-flex flex-column'>
+            <div className='d-flex flex-row'>
+                <div className='col-6 text-center'>
+                    Routine
+                </div>
+                <div className='col-6 text-center'>
+                    Event
+                </div>
+            </div>
             {
                 listEvents.map((item, index) => {
-                    if (index === 2) {
+                    if (index === getToday().week) {
                         console.log("list events", item);
                         return item.map((e, i) => {
                             return (
                                 <div key={e.id} className='d-flex flex-row'>
-                                <div className='col-6'>
-                                    {e.type === 'routine' ? `${e.title}` : ''}
+                                    <div className='routine-item'>
+                                        {e.type === 'routine' ? `${e.title}` : ''}
+                                    </div>
+                                    <div className='daily-tree-middle'></div>
+                                    <div className='event-item'>
+                                        {e.type === 'event' ? `${e.title}` : ''}
+                                    </div>
                                 </div>
-                                <div className='col-6'>
-                                    {e.type === 'event' ? `${e.title}` : ''}
-                                </div>
-                            </div>
                             )
                         })
                     }
                 })
             }
-        </div>    
-        
+        </div>
     ) 
 }
 
